d6016f718299d29f15a5b0d98ca5927b888cdb87
Francisco Nelsis <fhnelsis@outlook.com>
2015-04-12 23:24:51 -0300

    20151204 - Melhoria de layout rodapé e formPaciente.

--- consultarPaciente.php
+++ consultarPaciente.php
@@ -1,19 +1,20 @@
-<?php include ('includes/cabecalho.php') ?>
-<?php include ('includes/menu.php') ?>
+
+<?php include ('includes/cabecalho.php')?>
+<?php include ('includes/menu.php')?>
 
 
 <?php
-if (isset($_GET['id_exclusao'])) {
-    $con = mysqli_connect("localhost", "root", "", "sgpm");
-    mysqli_set_charset($con, "utf8");
-
-    //Inserir alert de confirmação
-    $sql = "DELETE FROM paciente WHERE id_paciente = {$_GET['id_exclusao']} ";
-    $exec = mysqli_query($con, $sql);
-
-    //Verificar alert de exclusão
-    $_SESSION['msg'] = 'Registro Excluído Com Sucesso!';
-    header('Location: consultarPaciente.php');
+if (isset ( $_GET ['id_exclusao'] )) {
+	$con = mysqli_connect ( "localhost", "root", "", "sgpm" );
+	mysqli_set_charset ( $con, "utf8" );
+	
+	// Inserir alert de confirma��o
+	$sql = "DELETE FROM paciente WHERE id_paciente = {$_GET['id_exclusao']} ";
+	$exec = mysqli_query ( $con, $sql );
+	
+	// Verificar alert de exclus�o
+	$_SESSION ['msg'] = 'Registro Excluído Com Sucesso!';
+	header ( 'Location: consultarPaciente.php' );
 }
 ?>
 <script type="text/javascript">
@@ -22,56 +23,64 @@ if (isset($_GET['id_exclusao'])) {
     });
 </script>
 <div class="tudo">
-    <div id="meio">
-        <div id="tituloPagina">
-            <h3>Buscar Paciente</h3>
-        </div>
+	<div id="meio">
+		<div id="tituloPagina">
+			<h3>Buscar Paciente</h3>
+		</div>
 
-        <div id="formBuscaPaciente">
+		<div id="formBuscaPaciente">
 
 
-            <div class="content-dataTable" style="width: 80%; margin: 0 auto; margin-top: -70px; margin-right: 70px">
+			<div class="content-dataTable"
+				style="width: 80%; margin: 0 auto; margin-top: -70px; margin-right: 70px">
                 <?php if (isset($_SESSION['msg'])) : ?>
                     <div class="alert alert-success" role="alert"><?php echo $_SESSION['msg']; ?></div>
                     <?php unset($_SESSION['msg']); ?>
                 <?php endif; ?>
-                <table id="pacientes" class="display" cellspacing="0" width="100%">
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>CPF</th>
-                            <th>Edição</th>
-                            <th>Exclusão</th>
-                        </tr>
-                    </thead>
+                <table id="pacientes" class="display" cellspacing="0"
+					width="100%">
+					<thead>
+						<tr>
+							<th>Nome</th>
+							<th>CPF</th>
+							<th>Edição</th>
+							<th>Exclusão</th>
+						</tr>
+					</thead>
 
-                    <tbody>
+					<tbody>
 
                         <?php
-                        $con = mysqli_connect("localhost", "root", "", "sgpm");
-                        mysqli_set_charset($con, "utf8");
-
-                        //Verificar essa query. Saber de onde ela pega o POST para a busca.
-                        $query = mysqli_query($con, "SELECT * FROM paciente");
-
-                        while ($linha = mysqli_fetch_array($query)) {
-                            ?>
+																								$con = mysqli_connect ( "localhost", "root", "", "sgpm" );
+																								mysqli_set_charset ( $con, "utf8" );
+																								
+																								// Verificar essa query. Saber de onde ela pega o POST para a busca.
+																								$query = mysqli_query ( $con, "SELECT * FROM paciente" );
+																								
+																								while ( $linha = mysqli_fetch_array ( $query ) ) {
+																									?>
 
                             <tr>
-                                <td><?php echo $linha['nome_paciente']; ?></td>
-                                <td><center><?php echo $linha['cpf']; ?></center></td>
-                        <td><center><a href="formPaciente.php?id=<?php echo $linha['id_paciente']; ?>">Editar</a></center></td>
-                        <td><center><a href="buscarPaciente.php?id_exclusao=<?php echo $linha['id_paciente']; ?>">Excluir</a></td></center>
+							<td><?php echo $linha['nome_paciente']; ?></td>
+							<td><center><?php echo $linha['cpf']; ?></center></td>
+							<td><center>
+									<a
+										href="formPaciente.php?id=<?php echo $linha['id_paciente']; ?>">Editar</a>
+								</center></td>
+							<td><center>
+									<a
+										href="buscarPaciente.php?id_exclusao=<?php echo $linha['id_paciente']; ?>">Excluir</a></td>
+							</center>
 
 
-                        </tr>
+						</tr>
 
 <?php } ?>
                     </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
+				</table>
+			</div>
+		</div>
+	</div>
 </div>
-<?php include ( 'includes/menuBack.php') ?>
+<?php include ( 'includes/menuBack.php')?>
 <?php include ('includes/rodape.php')?>
--- formPaciente.php
+++ formPaciente.php
@@ -1,84 +1,105 @@
-<?php include ('includes/cabecalho.php') ?>
-<?php include ('includes/menu.php') ?>
+<?php include ('includes/cabecalho.php')?>
+<?php include ('includes/menu.php')?>
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $con = mysqli_connect("localhost", "root", "", "sgpm");
-    mysqli_set_charset($con, "utf8");
 
-    $nome = $_POST['nome_paciente'];
-    $cpf = $_POST['cpf'];
-    $id = $_POST['id'];
-
-    if (empty($id)) {
-        $sql = "INSERT INTO paciente (nome_paciente, cpf) VALUES ('{$nome}', '{$cpf}')";
-    } else {
-        $sql = "UPDATE paciente SET nome_paciente = '{$nome}' , cpf = '{$cpf}' WHERE id_paciente = {$id} ";
-    }
-
-    $exec = mysqli_query($con, $sql);
-    $_SESSION['msg'] = 'Registro Salvo Com Sucesso!';
-    header('Location: consultarPaciente.php');
+if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
+	$con = mysqli_connect ( "localhost", "root", "", "sgpm" );
+	mysqli_set_charset ( $con, "utf8" );
+	
+	$nome = $_POST ['nome_paciente'];
+	$cpf = $_POST ['cpf'];
+	$id = $_POST ['id'];
+	
+	if (empty ( $id )) {
+		$sql = "INSERT INTO paciente (nome_paciente, cpf) VALUES ('{$nome}', '{$cpf}')";
+	} else {
+		$sql = "UPDATE paciente SET nome_paciente = '{$nome}' , cpf = '{$cpf}' WHERE id_paciente = {$id} ";
+	}
+	
+	$exec = mysqli_query ( $con, $sql );
+	$_SESSION ['msg'] = 'Registro Salvo Com Sucesso!';
+	header ( 'Location: consultarPaciente.php' );
 }
 
-
-//Se exitir um id passado por parâmetro
-if (isset($_GET['id'])) {
-    $con = mysqli_connect("localhost", "root", "", "sgpm");
-    mysqli_set_charset($con, "utf8");
-
-    $query = mysqli_query($con, "SELECT * FROM paciente WHERE id_paciente = {$_GET['id']} ");
-    $dadosPaciente = mysqli_fetch_array($query);
+// Se exitir um id passado por parâmetro
+if (isset ( $_GET ['id'] )) {
+	$con = mysqli_connect ( "localhost", "root", "", "sgpm" );
+	mysqli_set_charset ( $con, "utf8" );
+	
+	$query = mysqli_query ( $con, "SELECT * FROM paciente WHERE id_paciente = {$_GET['id']} " );
+	$dadosPaciente = mysqli_fetch_array ( $query );
 }
 ?>
 
 <div class="tudo">
-    <div id="meio">
-        <div id="tituloPagina">
-            <center><h3><?php echo isset($_GET['id']) ? "Alterar Paciente" : "Cadastrar Paciente"; ?></h3></center>
-        </div>
+	<div id="meio">
+		<div id="tituloPagina">
+			<center>
+				<h3><?php echo isset($_GET['id']) ? "Alterar Paciente" : "Cadastrar Paciente"; ?></h3>
+			</center>
+		</div>
 
-        <center>
-            <div id="formBuscaPaciente" >
-                <div class="content-dataTable" style="width: 40%; margin: 0 auto; margin-top: 200px; margin-right: 900px">
-                    <form method="POST" >
-                        <table width="500px">
-                            <tr >
-                                <td><label for="nome_paciente" >Nome: </label></td>
-                                <td><input style="width: 400px;" type="text" name="nome_paciente" id="nome_paciente"  maxlength="50" value="<?php if (isset($dadosPaciente['nome_paciente'])) {
-    echo $dadosPaciente['nome_paciente'];
-} ?>"/></td>
-                            </tr>
-                            <tr>
-                                <td><label for="cpf">CPF: </label></td>
-                                <td><input style="width: 150px;" type="text" name="cpf" id="cpf" maxlength="11" value="<?php if (isset($dadosPaciente['cpf'])) {
-    echo $dadosPaciente['cpf'];
-} ?>" /></td>
-                            </tr>
-                            <tr>
-                                <td><label for="cpf">RG: </label></td>
-                                <td><input type="text" name="rg" id="rg" maxlength="10" value="<?php if (isset($dadosPaciente['rg'])) {
-    echo $dadosPaciente['rg'];
-} ?>" /></td>
-                            </tr>
-                            <tr>
-                                <td><label for="cpf">CPF: </label></td>
-                                <td><input type="text" name="cpf" id="cpf" maxlength="11" value="<?php if (isset($dadosPaciente['cpf'])) {
-    echo $dadosPaciente['cpf'];
-} ?>" /></td>
-                            </tr>
-                        </table>
+		<center>
+			<div id="formBuscaPaciente">
+				<div class="content-dataTable"
+					style="width: 40%; margin: 0 auto; margin-top: 200px; margin-right: 900px">
+					<form method="POST">
+						<table width="500px">
+							<tr>
+								<td><label for="nome_paciente">Nome: </label></td>
+								<td><input style="width: 400px;" type="text"
+									name="nome_paciente" id="nome_paciente" maxlength="50"
+									value="<?php
+									
+if (isset ( $dadosPaciente ['nome_paciente'] )) {
+										echo $dadosPaciente ['nome_paciente'];
+									}
+									?>" /></td>
+							</tr>
+							<tr>
+								<td><label for="cpf">CPF: </label></td>
+								<td><input style="width: 150px;" type="text" name="cpf" id="cpf"
+									maxlength="11"
+									value="<?php
+									
+if (isset ( $dadosPaciente ['cpf'] )) {
+										echo $dadosPaciente ['cpf'];
+									}
+									?>" /></td>
+							</tr>
+							<tr>
+								<td><label for="cpf">RG: </label></td>
+								<td><input type="text" name="rg" id="rg" maxlength="10"
+									value="<?php
+									
+if (isset ( $dadosPaciente ['rg'] )) {
+										echo $dadosPaciente ['rg'];
+									}
+									?>" /></td>
+							</tr>
+							<tr>
+								<td><label for="cpf">CPF: </label></td>
+								<td><input type="text" name="cpf" id="cpf" maxlength="11"
+									value="<?php
+									
+if (isset ( $dadosPaciente ['cpf'] )) {
+										echo $dadosPaciente ['cpf'];
+									}
+									?>" /></td>
+							</tr>
+						</table>
 
-                        <input type="hidden" id="id" name="id" value="<?php
-if (isset($dadosPaciente['id_paciente'])) {
-    echo $dadosPaciente['id_paciente'];
-}
-?>" />
-                        <input type="submit" name="enviar" value="ENVIAR" />
-                    </form>
-                </div>
-            </div>
-        </center>
-    </div>
+						<input type="hidden" id="id" name="id"
+							value="<?php
+							if (isset ( $dadosPaciente ['id_paciente'] )) {
+								echo $dadosPaciente ['id_paciente'];
+							}
+							?>" /> <input type="submit" name="enviar" value="ENVIAR" />
+					</form>
+				</div>
+			</div>
+		</center>
+	</div>
 </div>
-<?php include ('includes/menuBack.php') ?>
-<?php include ('includes/rodape.php') ?>
+<?php include ('includes/menuBack.php')?>
+<?php include ('includes/rodape.php')?>
