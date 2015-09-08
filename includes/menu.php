fa19aa6a2495ad6ae352951d7f3fbb2bd123812e
Francisco Nelsis <fhnelsis@outlook.com>
2015-09-08 15:26:57 -0300

    20150908 - Atualização do menu e de permissões.

--- includes/menu.php
+++ includes/menu.php
@@ -1,9 +1,9 @@
-<div id='cssmenu' style="height: 575px; background-color: #3ab4a6; ">
-    <ul>
-        <li><a href='home.php'>Home</a></li>
+<div id='cssmenu' style="height: 575px; background-color: #3ab4a6;">
+	<ul>
+		<li><a href='home.php'>Home</a></li>
 
-        <li class='active'><a href='#'>Atendimentos</a>
-            <ul>
+		<li class='active'><a href='#'>Atendimentos</a>
+			<ul>
                 <?php if (verificarPermissao('ATENDIMENTO_LISTAR')): ?>
                     <li><a href='consultarAtendimento.php'>Buscar Atedimento</a></li>
                 <?php endif; ?>
@@ -11,19 +11,22 @@
                 <?php if (verificarPermissao('ATENDIMENTO_INSERIR')): ?>
                     <li><a href='formAtendimento.php'>Novo Atendimento</a>
                     <?php endif; ?>
+			</ul>
+		</li>
 
-                    <?php if (verificarPermissao('TIPO_ATENDIMENTO_LISTAR')): ?>
-                    <li><a href='consultarTipoAtendimento.php'>Tipos de Atendimento</a>
-                    <?php endif; ?>
+		<li class='active'><a href='#'>Tipos de Atendimentos</a>
+			<ul>
+				<?php if (verificarPermissao('TIPO_ATENDIMENTO_LISTAR')): ?>
+                  	<li><a href='consultarTipoAtendimento.php'>Tipos de Atendimento</a>
+                 <?php endif; ?>
+                 <?php if (verificarPermissao('TIPO_ATENDIMENTO_INSERIR')): ?>
+					<li><a href='formTipoAtendimento.php'>Novo Tipo de Atendimento</a>
+                 <?php endif; ?>
+			</ul>
+		</li>
 
-                    <?php if (verificarPermissao('TIPO_ATENDIMENTO_INSERIR')): ?>
-                    <li><a href='formTipoAtendimento.php'>Novo Tipo de Atendimento</a>
-                    <?php endif; ?>
-
-            </ul></li>
-
-        <li class='active'><a href='#'>Pacientes</a>
-            <ul>
+		<li class='active'><a href='#'>Pacientes</a>
+			<ul>
                 <?php if (verificarPermissao('PACIENTE_LISTAR')): ?>
                     <li><a href='consultarPaciente.php'>Buscar Paciente</a></li>
                 <?php endif; ?>
@@ -31,29 +34,31 @@
                 <?php if (verificarPermissao('PACIENTE_INSERIR')): ?>
                     <li><a href='formPaciente.php'>Novo Paciente</a>
                     <?php endif; ?>
-            </ul></li>
+			</ul></li>
 
-        <li class='active'><a href='#'>Funcionários</a>
-            <ul>
+		<li class='active'><a href='#'>Funcionários</a>
+			<ul>
                 <?php if (verificarPermissao('FUNCIONARIO_LISTAR')): ?>
-                    <li><a href='consultarFuncionario.php'>Buscar Funcionário</a></li>
+                    <li><a href='consultarFuncionario.php'>Buscar
+						Funcionário</a></li>
                 <?php endif; ?>
 
                 <?php if (verificarPermissao('FUNCIONARIO_INSERIR')): ?>
                     <li><a href='formFuncionario.php'>Novo Funcionário</a></li>
                 <?php endif; ?>
             </ul></li>
-        <li class='active'><a href='#'>Relatórios</a>
-            <ul>
+		<li class='active'><a href='#'>Relatórios</a>
+			<ul>
                 <?php if (verificarPermissao('RELATORIO')): ?>
-                    <li><a href='relatoriosAdministrativos.php'>Relatórios Administrativos</a></li>
+                    <li><a href='relatoriosAdministrativos.php'>Relatórios
+						Administrativos</a></li>
                 <?php endif; ?>
 
                 <?php if (verificarPermissao('RELATORIO')): ?>
                     <li><a href='#'>Relatórios Médicos</a></li>
                 <?php endif; ?>
             </ul></li>
-        <li><a href='#'>Sobre</a></li>
-        <li><a href="mailto:fhnelsis@outlook.com" target="_top">Contato</a></li>
-    </ul>
+		<li><a href='#'>Sobre</a></li>
+		<li><a href="mailto:fhnelsis@outlook.com" target="_top">Contato</a></li>
+	</ul>
 </div>
