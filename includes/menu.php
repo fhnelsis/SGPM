451922f42ee8e3c418a5fe203783ce978b1cd51e
fhnelsis <fhnelsis@outlook.com>
2015-09-30 14:14:31 -0300

    20150930 - Alteração no menu, para que ULs que não tenham permissões nenhuma, não sejam mostradas.

--- includes/menu.php
+++ includes/menu.php
@@ -2,6 +2,7 @@
 	<ul>
 		<li><a href='home.php'>Home</a></li>
 
+<?php if (verificarPermissao('ATENDIMENTO_LISTAR') || verificarPermissao('ATENDIMENTO_INSERIR')): ?>
 		<li class='active'><a href='#'>Atendimentos</a>
 			<ul>
                 <?php if (verificarPermissao('ATENDIMENTO_LISTAR')): ?>
@@ -12,22 +13,24 @@
                 <?php if (verificarPermissao('ATENDIMENTO_INSERIR')): ?>
                     <li><a href='formAtendimento.php'>Novo Atendimento</a>
                     <?php endif; ?>
-			
 			</ul></li>
+<?php endif; ?>
 
+<?php if (verificarPermissao('TIPO_ATENDIMENTO_LISTAR') || verificarPermissao('TIPO_ATENDIMENTO_INSERIR')): ?>
 		<li class='active'><a href='#'>Tipos de Atendimentos</a>
 			<ul>
 				<?php if (verificarPermissao('TIPO_ATENDIMENTO_LISTAR')): ?>
                   	<li><a href='consultarTipoAtendimento.php'>Tipos de
 						Atendimento</a>
                  <?php endif; ?>
+                 
                  <?php if (verificarPermissao('TIPO_ATENDIMENTO_INSERIR')): ?>
-									
 				<li><a href='formTipoAtendimento.php'>Novo Tipo de Atendimento</a>
                  <?php endif; ?>
-					
 			</ul></li>
+<?php endif; ?>
 
+<?php if (verificarPermissao('PACIENTE_LISTAR') || verificarPermissao('PACIENTE_INSERIR')): ?>
 		<li class='active'><a href='#'>Pacientes</a>
 			<ul>
                 <?php if (verificarPermissao('PACIENTE_LISTAR')): ?>
@@ -38,7 +41,9 @@
                     <li><a href='formPaciente.php'>Novo Paciente</a>
                     <?php endif; ?>
 			</ul></li>
+<?php endif; ?>
 
+<?php if (verificarPermissao('FUNCIONARIO_LISTAR') || verificarPermissao('FUNCIONARIO_INSERIR')): ?>
 		<li class='active'><a href='#'>Funcionários</a>
 			<ul>
                 <?php if (verificarPermissao('FUNCIONARIO_LISTAR')): ?>
@@ -50,7 +55,9 @@
                     <li><a href='formFuncionario.php'>Novo Funcionário</a></li>
                 <?php endif; ?>
             </ul></li>
+<?php endif; ?>
 
+<?php if (verificarPermissao('UBS_LISTAR') || verificarPermissao('UBS_INSERIR')): ?>
 		<li class='active'><a href='#'>UBS</a>
 			<ul>
                 <?php if (verificarPermissao('UBS_LISTAR')): ?>
@@ -61,17 +68,20 @@
                     <li><a href='formUBS.php'>Cadastrar UBS</a></li>
                 <?php endif; ?>
             </ul></li>
+<?php endif; ?>
 
+<?php if (verificarPermissao('RELATORIO') || verificarPermissao('RELATORIO')): ?>
 		<li class='active'><a href='#'>Relatórios</a>
 			<ul>
-                <?php if (verificarPermissao('RELATORIO')): ?>
-                    <li><a href='relatoriosAdministrativos.php'>Relatórios
-						Administrativos</a></li>
+			    <?php if (verificarPermissao('RELATORIO')): ?>
+                    <li><a href='relatoriosAdministrativos.php'>Relatórios Administrativos</a></li>
                 <?php endif; ?>
 
                 <?php if (verificarPermissao('RELATORIO')): ?>
                     <li><a href='#'>Relatórios Médicos</a></li>
                 <?php endif; ?>
+                
             </ul></li>
+<?php endif; ?>  
 	</ul>
 </div>
