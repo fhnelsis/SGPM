8e20edde4f7cdd439ccfae3b067f00d452e91d28
Eduardo Praxedes <praxeduardo@gmail.com>
2016-06-13 00:06:18 -0300

    Ajuste Agenda

--- consultarPacienteCpf.php
+++ consultarPacienteCpf.php
@@ -1,10 +1,11 @@
 <?php
+session_start();
 
 if ($_REQUEST['cpf_buscado']) {
     $con = mysqli_connect("localhost", "root", "", "sgpm");
     mysqli_set_charset($con, "utf8");
 
-    $queryCpfPaciente = mysqli_query($con, "SELECT * FROM paciente WHERE cpf = '{$_POST['cpf_buscado']}' ");
+    $queryCpfPaciente = mysqli_query($con, "SELECT * FROM paciente WHERE cpf = '{$_POST['cpf_buscado']}' AND id_ubs = '{$_SESSION['LOGIN']['UBS']}' ");
     $paciente = mysqli_fetch_array($queryCpfPaciente);
     echo json_encode($paciente);
     die();
